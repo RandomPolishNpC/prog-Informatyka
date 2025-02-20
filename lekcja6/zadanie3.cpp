a4f4dff2f01f799aa24fccca8c04cf6c084ffb3c
MarcinReiman <mreiman702@technikumkreatywne>
2025-02-20 09:58:18 +0100

    komentarz

--- lekcja6/zadanie3.cpp
+++ lekcja6/zadanie3.cpp
@@ -3,23 +3,17 @@ using namespace std;
 int main(){
 	int b[10];
 	int z;
-	int h = 0;
-	int p = 0;
 	for(int i = 0;i != 10;i++){
-		cout <<"podaj liczbe nr."<<i+1<<endl;
+		cout <<"podaj liczbe nr "<<i+1<<": ";
 		cin >> b[i];
 	}
-	for(int g = 0;g != 10;g++){
-		z += b[g];
-	}
-	z -= (z%2);
-	int r = (z/2);
 	cout <<"liczby parzyste: ";
-	for(int w = 0;w != r;w++){
-		h += 2;
-		cout <<h<<" ";
-		p += h;
+	for(int g = 0;g != 10;g++){
+		if(0==(b[g]%2)){
+			cout <<b[g]<<", ";
+			z += b[g];
+		}
 	}
-	cout <<endl<<"suma liczb parzystych: "<<p;
+	cout <<endl<<"suma liczb parzystych: "<<z;
 	return 0;
 }
